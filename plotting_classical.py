917d1bd0af53d40131526fa0e4d0081351e696cd
Sébastien Maillé <smail031@uottawa.ca>
2021-09-07 16:50:50 -0400

    Now using timestamps instead of lick indices

--- plotting_classical.py
+++ plotting_classical.py
@@ -21,51 +21,68 @@ x_limit = 5000
 if date_experiment == 'today':
     date_experiment = time.strftime("%Y-%m-%d", time.localtime(time.time()))
 
-file = f'/Volumes/GoogleDrive/Shared drives/Beique Lab/Data/Raspberry PI Data/Sebastien/Dual_Lickport/Mice/{mouse_number}/{date_experiment}/ms{mouse_number}_{date_experiment}_block{block_number}.hdf5'
+file = ('/Volumes/GoogleDrive/Shared drives/Beique Lab/Data/'
+        'Raspberry PI Data/Sebastien/Dual_Lickport/Mice/'
+        f'{mouse_number}/{date_experiment}/'
+        f'ms{mouse_number}_{date_experiment}_block{block_number}.hdf5')
 
 f = h5py.File(file, 'r') #open HDF5 file
-
-
 
 plt.style.use('seaborn-pastel')
 fig, ax = plt.subplots(figsize = (8, 6), constrained_layout = True)
 #gs = gridspec.GridSpec(nrows = 1, ncols = 2, width_ratios = [1, 1], figure = fig)
 
-
 #################
 
-num_trials = len(f['lick_l']['volt']) #get number of trials
+num_trials = len(f['lick_l']['volt'])
 
-L_licks = np.zeros(num_trials, dtype=np.ndarray) #Will store left licks
-R_licks = np.zeros(num_trials, dtype=np.ndarray) #Will store right licks
-
-plt.eventplot(np.expand_dims(f['rew_l']['t'], 1), lw=2.5, linelengths=0.8, color='black') #create raster of L reward times
-plt.eventplot(np.expand_dims(f['rew_r']['t'], 1),lw=2.5,  linelengths=0.8, color='black') #create raster of R reward times
+# Create a raster plot of reward delivery times
+plt.eventplot(np.expand_dims(f['rew_l']['t'], 1),
+              lw=2.5, linelengths=0.8, color='black')
+plt.eventplot(np.expand_dims(f['rew_r']['t'], 1),
+              lw=2.5,  linelengths=0.8, color='black')
 
 for trial in range(num_trials):
 
-    L_lick_v = f['lick_l']['volt'][trial] #raw voltage traces from left lickport
-    R_lick_v = f['lick_r']['volt'][trial] #raw voltage traces from right lickport
+    # Get lick indices by finding where derivative of lickport voltage is > 0.
+    L_lick_d = np.diff(f['lick_l']['volt'][trial])
+    R_lick_d = np.diff(f['lick_r']['volt'][trial])
+    L_lick_index = np.argwhere(L_lick_d > 0).flatten()
+    R_lick_index = np.argwhere(R_lick_d > 0).flatten()
 
-    L_lick_d = np.diff(L_lick_v) #1st derivative of L lickport voltage
-    R_lick_d = np.diff(R_lick_v) #1st derivative of R lickport voltage
+    # Get lick limes by looking at timestamps for associated lick indices
+    L_lick_timestamps = np.empty(len(L_lick_index), dtype=float)
+    R_lick_timestamps = np.empty(len(R_lick_index), dtype=float)
 
-    L_licks[trial] = np.argwhere(L_lick_d > 0).flatten() #licks occur where lick_d is above 0
-    R_licks[trial] = np.argwhere(R_lick_d > 0).flatten()
+    for lick in range(len(L_lick_index)):    
+        L_lick_timestamps[lick] = (
+            f['lick_l']['t'][trial][L_lick_index[lick]])
     
-    ax.eventplot(L_licks[trial], lineoffsets=trial, linelengths=0.8, linewidths=0.9, color='blue') #create raster plot of L licks
-    ax.eventplot(R_licks[trial], lineoffsets=trial, linelengths=0.8, linewidths=0.9, color='red') #create raster plot of R licks
+    for lick in range(len(R_lick_index)):
+        R_lick_timestamps[lick] = (
+            f['lick_r']['t'][trial][R_lick_index[lick]])
 
-    sample_tone_on = f['sample_tone']['t'][trial] #Find sample tone start time
-    sample_tone_end = f['sample_tone']['end'][trial] #Find sample tone end time
+    ax.eventplot(L_lick_timestamps,
+                 lineoffsets=trial, linelengths=0.8, lw=0.9, color='blue')
+    ax.eventplot(R_lick_timestamps,
+                 lineoffsets=trial, linelengths=0.8, lw=0.9, color='red')
+
+    # Create a shaded area indicating where sample tones are played.
+    sample_tone_on = f['sample_tone']['t'][trial]
+    sample_tone_end = f['sample_tone']['end'][trial]
+
+    ax.eventplot([sample_tone_end+250],
+                 lineoffsets=trial, linelengths=1, lw=0.9, color='grey')
 
     if 'L' in str(f['sample_tone']['type'][trial]): #if L trial
+        ax.fill_between([sample_tone_on, sample_tone_end],
+                        [trial-0.5, trial-0.5], [trial+0.5, trial+0.5],
+                        edgecolor='black',lw=0.5, facecolor='blue', alpha=0.3)
 
-        ax.fill_between([sample_tone_on, sample_tone_end], [trial-0.5, trial-0.5], [trial+0.5, trial+0.5], edgecolor='black',lw=0.5, facecolor='blue', alpha=0.3) #Show where the tone was played
-
-    elif 'R' in str(f['sample_tone']['type'][trial]): #if R trial
-                              
-        ax.fill_between([sample_tone_on, sample_tone_end], [trial-0.5, trial-0.5], [trial+0.5, trial+0.5], edgecolor='black',lw=0.5, facecolor='red', alpha=0.3)
+    elif 'R' in str(f['sample_tone']['type'][trial]): #if R trial                     
+        ax.fill_between([sample_tone_on, sample_tone_end],
+                        [trial-0.5, trial-0.5], [trial+0.5, trial+0.5],
+                        edgecolor='black',lw=0.5, facecolor='red', alpha=0.3)
 
 
 
